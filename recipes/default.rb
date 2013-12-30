357c2cc99146e769e637f3f26d5686926453c5c5
Ubuntu <ubuntu@ip-10-0-0-227.us-west-1.compute.internal>
2013-12-30 06:23:51 +0000

    Correct path

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,7 +11,7 @@ include_recipe "logrotate::default"
 
 logrotate_app 'kafka' do
   cookbook  'logrotate'
-  path      ['/var/log/kafka/server.log', '/var/log/kafka/server.err']
+  path      ['/var/log/kafka/server.out', '/var/log/kafka/server.err']
   frequency 'daily'
   rotate    7
   options   ['missingok']
@@ -19,19 +19,19 @@ end
 
 logrotate_app 'kafka-zoo' do
   cookbook  'logrotate'
-  path      ['/var/log/kafka/zookeeper.log', '/var/log/kafka/zookeeper.err']
+  path      ['/var/log/kafka/zookeeper.out', '/var/log/kafka/zookeeper.err']
   frequency 'daily'
   rotate    7
   options   ['missingok']
 end
 
-logrotate_app 'stunnel' do
-  cookbook  'logrotate'
-  path      ['/var/log/stunnel/stunnel.log']
-  frequency 'daily'
-  rotate    7
-  options   ['missingok']
-end
+#logrotate_app 'stunnel' do
+#  cookbook  'logrotate'
+#  path      ['/var/log/stunnel/stunnel.log']
+#  frequency 'daily'
+#  rotate    7
+#  options   ['missingok']
+#end
 
 logrotate_app 'jmxtrans' do
   cookbook  'logrotate'
